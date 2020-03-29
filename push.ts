983ef333bf0e10adc82f5c9eccfd0e5c6f936810
ufoo68 <mitarou.yuutarou@gmail.com>
2020-03-29 14:29:06 +0900

    Refactoring

--- push.ts
+++ push.ts
@@ -1,6 +1,6 @@
 import { buildReplyText } from 'line-message-builder'
 import * as Line from '@line/bot-sdk'
-import { commute, book, window } from './research'
+import { research } from './research'
 
 const channelAccessToken = process.env.ACCESS_TOKEN!
 const channelSecret = process.env.CHANNEL_SECRET!
@@ -16,21 +16,33 @@ switch (process.argv[2]) {
     client.pushMessage(process.env.USER_ID!, {
       type: 'flex',
       altText: 'research',
-      contents: commute
+      contents: research(
+        '今週は出社予定ですか？',
+        'https://3.bp.blogspot.com/-TRUxVT2290k/WI1zUd8sq0I/AAAAAAABBX0/6USLXDjtWxAtKQLxJr-InNGM2s_N5sjugCLcB/s800/train_manin_business.png',
+        'commute',
+      )
     })
     break
   case 'book':
     client.pushMessage(process.env.USER_ID!, {
       type: 'flex',
       altText: 'research',
-      contents: book
+      contents: research(
+        '本を読む機会が増えましたか？',
+        'https://2.bp.blogspot.com/-JAIbq3c1d4I/UPzH_qDWlUI/AAAAAAAAK04/Ys-vw2CpZbg/s400/reading_girl.png',
+        'book',
+      )
     })
     break
   case 'window':
     client.pushMessage(process.env.USER_ID!, {
       type: 'flex',
       altText: 'research',
-      contents: window
+      contents: research(
+        '窓を開けましたか？',
+        'https://1.bp.blogspot.com/-O0pTV6HlFm4/W0mGQji79XI/AAAAAAABNYI/-0EJnxDOackcZXAyS6QVhsoJjTw451EJgCLcBGAs/s800/window_open_boy.png',
+        'window',
+      )
     })
     break
 }
